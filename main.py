3ca8c37bf32b16231558df9560d4e3795de4cc2a
robgriffin247 <robgriffin247@hotmail.com>
2025-03-19 13:12:39 +0100

    adding swapi call

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import httpx
 
 
 def api_wan(resource='films'):
-    response = httpx.get(f'https://swapi.dev/api/')
+    
+    resource = resource.lower()
+
+    if resource not in ['films', 'people', 'planets', 'starships', 'vehicles', 'species']:
+        raise ValueError('Resource type must be films, people, planets, starships, vehicles or species')
+    
+    response = httpx.get(f'https://swapi.dev/api/{resource}/')
+    
     response.raise_for_status()
 
-    return response
+    return response.data
 
 if __name__ == "__main__":
-    #print(api_wan(sys.argv[0]))
-    print(sys.argv)
+    print(api_wan(sys.argv[1]))
