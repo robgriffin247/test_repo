ed7af1a0587ad4f5664e3d94edbd66a030e634df
robgriffin247 <robgriffin247@hotmail.com>
2025-03-19 13:17:27 +0100

    added raises

--- main.py
+++ main.py
@@ -2,18 +2,32 @@ import sys
 import httpx
 
 
-def api_wan(resource='films'):
-    
+def api_wan(resource="films"):
+
     resource = resource.lower()
 
-    if resource not in ['films', 'people', 'planets', 'starships', 'vehicles', 'species']:
-        raise ValueError('Resource type must be films, people, planets, starships, vehicles or species')
-    
-    response = httpx.get(f'https://swapi.dev/api/{resource}/')
-    
+    if resource not in [
+        "films",
+        "people",
+        "planets",
+        "starships",
+        "vehicles",
+        "species",
+    ]:
+        raise ValueError(
+            "Resource type must be films, people, planets, starships, vehicles or species"
+        )
+
+    response = httpx.get(f"https://swapi.dev/api/{resource}/")
+
     response.raise_for_status()
 
-    return response.json
+    json_data = response.json()
+
+    next_url = response["next"]
+
+    return next_url
+
 
 if __name__ == "__main__":
     print(api_wan(sys.argv[1]))
