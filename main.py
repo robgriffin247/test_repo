aef1015842b6776f608e3dc5c8eeb671710ad57f
robgriffin247 <robgriffin247@hotmail.com>
2025-03-19 13:31:53 +0100

    added logging

--- main.py
+++ main.py
@@ -1,33 +1,6 @@
-import sys
-import httpx
-
-
-def api_wan(resource="films"):
-
-    resource = resource.lower()
-
-    if resource not in [
-        "films",
-        "people",
-        "planets",
-        "starships",
-        "vehicles",
-        "species",
-    ]:
-        raise ValueError(
-            "Resource type must be films, people, planets, starships, vehicles or species"
-        )
-
-    response = httpx.get(f"https://swapi.dev/api/{resource}/")
-
-    response.raise_for_status()
-
-    json_data = response.json()
-
-    next_url = json_data["next"]
-
-    return json_data["results"]
-
+import datetime
 
 if __name__ == "__main__":
-    print(api_wan(sys.argv[1]))
+    with open("log.txt", "w") as f:
+        now = datetime.datetime.now()
+        f.write(f"Updated {now}\n".encode())
