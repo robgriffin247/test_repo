b09e763a3ed68431e532224a4f19454da6a2bac2
robgriffin247 <robgriffin247@hotmail.com>
2025-03-19 13:39:38 +0100

    fixed now string

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
 
 if __name__ == "__main__":
-    with open("log.txt", "w") as f:
-        now = datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
-        f.write(f"Updated {now}\n".encode())
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    with open("log.txt", "w") as file:
+        file.write(f"{now}")
