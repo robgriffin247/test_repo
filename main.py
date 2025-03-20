fd2c239eaa0c803cadc821ffc27ad4dabf271af8
robgriffin247 <robgriffin247@hotmail.com>
2025-03-20 07:45:13 +0100

    remove logging

--- main.py
+++ main.py
@@ -2,6 +2,4 @@ import datetime
 
 if __name__ == "__main__":
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    with open("actions_log.txt", "w") as file:
-        file.write(f"{now}\n")
+    print(f"Run complete: {now}")
